4cfde659d1dd62f76a4d8679ed2f643ad53d097d
Preeternal <preeternal@ya.ru>
2019-05-21 20:24:02 +0300

    eslint comment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-console */
 import "@babel/polyfill/noConflict";
 import cron from "node-cron";
 import server from "./server";
 import cbr from "./cbr";
 
 server.start({ port: process.env.PORT || 4000 }, () => {
-  // eslint-disable-next-line no-console
   console.log("The server is up!");
 });
 
 cron.schedule("0-59 * * * *", () => {
-  // eslint-disable-next-line no-console
   console.log("running cbr");
   cbr();
 });
