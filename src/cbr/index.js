7b416508fb54031dff7165621159a7b8a27c0c4e
Preeternal <preeternal@ya.ru>
2020-01-25 13:06:11 +0200

    don't save db changes into herokus

--- src/cbr/index.js
+++ src/cbr/index.js
@@ -69,12 +69,7 @@ const cbr = () => {
           // console.log(date2.valueOf());
           // console.dir(result.ValCurs.$.Date);
           // console.log(new Date());
-          // console.dir(result.ValCurs.Valute.length);
-          await prisma.mutation.createHeroku({
-            data: {
-              result: result.ValCurs.$.Date
-            }
-          });
+          // console.dir(result.ValCurs.Valute.length);         
           result.ValCurs.Valute.forEach(async element => {
             await prisma.mutation.upsertCurrency({
               where: {
